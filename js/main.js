9a5a41c35fb1d4b280306ab739b3f1c1a80976ed
Sanhita dhar <96383145+Sanhita-dhar@users.noreply.github.com>
2022-03-10 22:49:03 -0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,18 +1,16 @@
-//project testimonial slider code
-
 $('.owl-carousel').owlCarousel({
-			    loop:true,
-			    margin:10,
-			    nav:true,
-			    responsive:{
-			        0:{
-			            items:1
-			        },
-			        600:{
-			            items:2
-			        },
-			        1000:{
-			            items:3
-			        }
-			    }
-			})
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        780:{
+            items:1
+        },
+        1000:{
+            items:3
+        }
+    }
+})
